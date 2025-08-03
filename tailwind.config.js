a4bfcdbac017f37be21657156c3411942a027f86
TheManWhoCodesStuffSometimes <167024529+TheManWhoCodesStuffSometimes@users.noreply.github.com>
2025-08-03 14:26:59 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}', // Add this if you move components
+    // Remove the './src/**/*' line since you're not using src/
   ],
   theme: {
     extend: {},
