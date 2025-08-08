61e8633b6aab0ecee616bc5e8e9df1bef1fb80a7
TheManWhoCodesStuffSometimes <167024529+TheManWhoCodesStuffSometimes@users.noreply.github.com>
2025-08-08 11:54:44 -0600

    Create PlayIcon.tsx

---
+++ components/dj/icons/PlayIcon.tsx
@@ -0,0 +1,81 @@
+// components/dj/icons/PlayIcon.tsx
+import React from 'react'
+
+const PlayIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={className}
+  >
+    <path d="M8 5v14l11-7z" />
+  </svg>
+)
+
+export default PlayIcon
+
+// components/dj/icons/ClockIcon.tsx
+import React from 'react'
+
+const ClockIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+)
+
+export default ClockIcon
+
+// components/dj/icons/BlacklistIcon.tsx
+import React from 'react'
+
+const BlacklistIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+    />
+  </svg>
+)
+
+export default BlacklistIcon
+
+// components/dj/icons/TrashIcon.tsx
+import React from 'react'
+
+const TrashIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={className}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.134-2.036-2.134H8.716C7.58 2.75 6.67 3.704 6.67 4.884v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+    />
+  </svg>
+)
+
+export default TrashIcon
