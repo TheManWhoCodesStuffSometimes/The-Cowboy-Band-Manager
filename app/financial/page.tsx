37712bb3f62604f6569cd9daa2949327cdb9d7f3
TheManWhoCodesStuffSometimes <167024529+TheManWhoCodesStuffSometimes@users.noreply.github.com>
2025-08-24 14:17:18 -0600

    Create page.tsx

---
+++ app/financial/page.tsx
@@ -0,0 +1,6 @@
+// app/financial/page.tsx - Financial Analysis Page
+import FinancialDashboard from '@/components/financial/FinancialDashboard'
+
+export default function FinancialPage() {
+  return <FinancialDashboard />
+}
